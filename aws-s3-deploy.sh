e79a79121fb9f4e58c74c676ac0cc483cd4c3133
Jeff Matthews <jmatthews@affinipay.com>
2017-01-09 13:54:01 -0600

    cleanup

--- aws-s3-deploy.sh
+++ aws-s3-deploy.sh
@@ -12,30 +12,35 @@ ________                .__                 ________
 EOF
 
 # debugging
- set -xv
+set -xv
 
 # To run this script, you must:
 #   - Sign up for an AWS S3 account
 #   - Have access to the relevant AWS S3 buckets
 #   - Set up your AWS CLI environment (http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-set-up.html)
 
-echo Do you want to deploy the latest tarball?
+echo Do you want to deploy the latest build?
 select yn in "Yes" "No"; do
   case $yn in
     Yes)
-    echo Downloading the latest tarball...
+    echo Downloading the latest tarball from AWS S3...
+
     # find the latest tarball, print only the filename, and set as a variable
     file=$(aws s3 ls s3://affinipay-qa/assets/dev-docs/master/ | sort -n | tail -1 | awk '{print $4}')
+
     # download the file by passing the variable to the aws s3 cp command
     aws s3 cp s3://affinipay-qa/assets/dev-docs/master/$file .;
-    echo Deploying the latest tarball to the web server...
+
     # extract tarball to web server directory
-    sudo tar -xvzf $file -C /home/affinipay/www/doc-site --overwrite
-    echo Successfully deployed the latest tarball!
+    # echo Deploying the latest tarball to the web server...
+    # sudo tar -xvzf $file -C /home/affinipay/www/doc-site
     break;;
-    No) If No, you must manually enter the filename associated with the tarball you want to deploy.
+
+    No)
+    echo If No, you must manually enter the filename associated with the tarball you want to deploy.
     echo Enter the filename of the tarball you want to deploy and press enter:
     read filename
+    # download the specified tarball
     aws s3 cp s3://affinipay-qa/assets/dev-docs/master/$filename .;
     exit;;
   esac
