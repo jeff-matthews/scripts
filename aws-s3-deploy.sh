ac94b89eafc06841a791def64b7566d4f740c069
Jeff Matthews <jmatthews@affinipay.com>
2017-01-10 14:22:23 -0600

    added some color to the prompts

--- aws-s3-deploy.sh
+++ aws-s3-deploy.sh
@@ -4,15 +4,15 @@ cat << "EOF"
 
 ________                .__                     __________
 \______ \   ____ ______ |  |   ____ ___.__. ____\______   \
- |   |   \_/ __ \\____ \|  |  /  _ <   |  |/ __ \|       _/
- |   |    \  ___/|  |_> >  |_(  <_> )___  \  ___/|    |   \
-/_______  /\___  >   __/|____/\____// ____|\___  >____|_  /
+ |   |   \_/ __ \\____ \|  |  /  _ <   |  |/ __ \|      __/
+ |   |    \  ___/|  |_> >  |_(  <_> )___  \  ___/|   |    \
+/_______  /\___  >   __/|____/\____// ____|\___  >___|__  /
         \/     \/|__|               \/         \/       \/
 
 EOF
 
 # debugging
-# set -xv
+set -xv
 
 # set "fail on error" in bash
 set -e
@@ -22,7 +22,15 @@ set -e
 #   - Have access to the relevant AWS S3 buckets
 #   - Set up your AWS CLI environment (http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-set-up.html)
 
-echo Which branch do you want to deploy?
+red='\033[1;31m'
+green='\033[1;32m'
+yellow='\033[1;33m'
+purple='\033[1;34m'
+magenta='\033[1;35m'
+cyan='\033[1;36m'
+white='\033[1;37m'
+
+echo -e ${white}Which branch do you want to deploy? ${cyan}
   select branch in "master" "qa" "production"; do
     case $branch in
       master) master=s3://affinipay-qa/assets/dev-docs/master
@@ -31,54 +39,61 @@ echo Which branch do you want to deploy?
       break;;
       production) production=s3://affinipay-qa/assets/dev-docs/production
       break;;
-      *) echo Invalid option! Enter 1, 2, or 3.
+      *) echo -e ${red}Invalid option! Enter 1, 2, or 3.
     esac
   done
 
-echo Do you want to deploy the latest build? If No, you must manually enter the filename associated with the tarball you want to deploy.
+echo -e ${white}Do you want to deploy the latest build? If No, you must manually enter the filename associated with the tarball you want to deploy. ${cyan}
 select yn in "Yes" "No"; do
   case $yn in
     Yes)
-    echo Downloading the latest tarball from AWS S3...
+    echo -e ${yellow}Downloading the latest tarball from AWS S3...
 
     if [ "$branch" = "master" ]; then
       # find the latest tarball, print only the filename, and set as a variable
       file=$(aws s3 ls $master/ | sort -n | tail -1 | awk '{print $4}')
       # download the file by passing the variable to the aws s3 cp command
       aws s3 cp $master/$file .;
+      echo -e ${green}"Download complete!"
     elif [ "$branch" = "qa" ]; then
       # find the latest tarball, print only the filename, and set as a variable
       file=$(aws s3 ls $qa/ | sort -n | tail -1 | awk '{print $4}')
       # download the file by passing the variable to the aws s3 cp command
       aws s3 cp $qa/$file .;
+      echo -e ${green}"Download complete!"
     else [ "$branch" = "production" ]
       # find the latest tarball, print only the filename, and set as a variable
       file=$(aws s3 ls $production/ | sort -n | tail -1 | awk '{print $4}')
       # download the file by passing the variable to the aws s3 cp command
       aws s3 cp $production/$file .;
+      echo -e ${green}"Download complete!"
     fi
 
     # extract tarball to web server directory
-    # echo Deploying the latest tarball to the web server...
+    # echo -e ${yellow} Deploying the latest tarball to the web server...
     # sudo tar -xvzf $file -C /home/affinipay/www/doc-site
 
     break;;
 
     No)
-    read -p "Enter the filename of the tarball you want to deploy and press enter: " filename
+    echo -n -e ${white}"Enter the filename of the tarball you want to deploy and press [Enter]: "
+    read filename
 
     if [ "$branch" = "master" ]; then
       # download the specified tarball
       aws s3 cp $master/$filename .;
+      echo -e ${green}"Download complete!"
     elif [ "$branch" = "qa" ]; then
       aws s3 cp $qa/$filename .;
+      echo -e ${green}"Download complete!"
     else [ "$branch" = "production" ]
       aws s3 cp $production/$filename .;
+      echo -e ${green}"Download complete!"
     fi
 
     break;;
 
-    *) echo Invalid option! Enter 1 or 2.
+    *) echo -e ${red}Invalid option! Enter 1 or 2.
 
   esac
 done
