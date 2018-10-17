e3ad6d7e971e7a45c451083a540d6c52d3a2cb65
Jeff Matthews <matthews.jeffery@gmail.com>
2018-10-17 14:30:06 -0500

    Magento merchdoc deployment (#2)
    * Initial commit; working draft
    
    * Fine tuning

---
+++ aws-s3-deploy-magento-docs.sh
@@ -0,0 +1,157 @@
+#!/bin/bash
+
+# This script automates the following steps:
+#   1. Prompt user to choose an environment (e.g., personal, staging, production).
+#   2. Prompt user to choose a version (e.g., 2.0, 2.1, 2.2).
+#   3. Prompt user to choose a guide (e.g., CE, EE, B2B).
+#   4. Copy files between environments based on choices.
+
+# To run this script, you must:
+#   - Generate AWS S3 access keys
+#   - Download and install the AWS CLI tool
+#   - Configure your AWS CLI tool with your AWS access keys
+# See https://wiki.corp.magento.com/display/MERCHDOCS/Working+with+S3.
+
+cat << "EOF"
+
+________                .__                     __________
+\______ \   ____ ______ |  |   ____ ___.__. ____\______   \
+ |   |   \_/ __ \\____ \|  |  /  _ <   |  |/ __ \|      __/
+ |   |    \  ___/|  |_> >  |_(  <_> )___  \  ___/|   |    \
+/_______  /\___  >   __/|____/\____// ____|\___  >___|__  /
+        \/     \/|__|               \/         \/       \/
+
+EOF
+
+# debugging
+# set -x
+
+# set "fail on error" in bash
+set -e
+
+red='\033[1;31m'
+green='\033[1;32m'
+yellow='\033[1;33m'
+purple='\033[1;34m'
+magenta='\033[1;35m'
+cyan='\033[1;36m'
+white='\033[1;37m'
+
+########################
+# Choose an environment# 
+########################
+echo -e ${white}"Which environment do you want to deploy?" ${cyan}
+  select environment in "personal" "staging" "production"; do
+    case $environment in
+      personal) personal="s3://docs.magedevteam.com"
+      break;;
+      staging) staging="s3://docs.magedevteam.com/staging"
+      break;;
+      production) production="s3://docs.magento.com"
+      break;;
+      *) echo -e ${red}Invalid option! Enter 1, 2, or 3.
+    esac
+  done 
+
+# ####################
+# # Choose a version # 
+# ####################
+# echo -e ${white}"Which version do you want to deploy?" ${cyan}
+#   select version in 2.0 2.1 2.2; do
+#     case $version in
+#       2.0) twoo="2.0"
+#       break;;
+#       2.1) twoone="2.1"
+#       break;;
+#       2.2) twotwo="/"
+#       break;;
+#       *) echo -e ${red}Invalid option! Enter 1, 2, or 3.
+#     esac
+#   done
+
+# ##################
+# # Choose a Guide # 
+# ##################
+# echo -e ${white}"Which guide do you want to deploy?" ${cyan}
+#   select guide in CE EE B2B; do
+#     case $guide in
+#       CE) CE="ce"
+#       break;;
+#       EE) EE="ee"
+#       break;;
+#       B2B) B2B="b2b"
+#       break;;
+#       *) echo -e ${red}Invalid option! Enter 1, 2, or 3.
+#     esac
+#   done
+
+# # Test
+# if [ "$environment" = "personal" ] && [ "$version" = "twotwo" ]; then
+#   aws s3 ls $personal/$version/$guide/;
+# fi
+# break
+
+##############################
+# Choose a deployment method #
+##############################
+echo -e ${white}"Do you want to deploy files from your local file system? Enter "No" to copy files between s3 directories/buckets." ${cyan}
+select yn in "Yes" "No"; do
+  case $yn in
+    Yes)
+    if [ "$environment" = "personal" ]; then
+      # deploy all local files from the current directory to your personal directory on the S3 staging bucket
+      echo -e ${white}"Enter the name of your personal s3 directory and press [ENTER]: "
+      read name
+      echo -e ${yellow}"Uploading the contents of the current local directory to your personal S3 staging environment..."
+      # pass the s3 path ($name) to the aws s3 sync command
+      aws s3 sync . $personal/$name/ --exclude ".git/" --dryrun;
+      echo -e ${green}"Deployment complete!"
+    elif [ "$environment" = "staging" ]; then
+      echo -e ${yellow}"Uploading the contents of the current local directory to the main S3 staging environment..."
+      # deploy all local files from the current directory to staging
+      aws s3 sync . $staging/ --exclude ".git/" --dryrun;
+      echo -e ${green}"Deployment complete!"
+      exit
+    else [ "$environment" = "production" ]
+      echo -e ${yellow}"Uploading the contents of the current local directory to the S3 production environment..."
+      # deploy all local files from the current directory to production
+      aws s3 sync . $production/ --exclude ".git/" --dryrun;
+      echo -e ${green}"Deployment complete!"
+    fi
+    break;;
+    No)
+    echo -e ${white}"Do you want to copy files between directories on staging? Enter "No" to deploy production." ${cyan}
+    select list in "Yes" "No"; do
+      case $list in
+        Yes)
+        if [ "$environment" = "personal" ]; then
+          # specify which directory to copy FROM
+          echo -e ${white}"Enter the name of the s3 directory you want to copy files FROM and press [ENTER]: " ${cyan}
+          read custom
+          # specify which directory to copy TO
+          echo -e ${white}"Enter the name of the s3 directory you want to copy files TO and press [ENTER]: " ${cyan}
+          read name
+          # copy files from the custom directory to the personal directory
+          aws s3 sync s3://docs.magedevteam.com/$custom/ s3://docs.magedevteam.com/$name/ --exclude ".git/" --dryrun;
+          echo -e ${green}"Deployment complete!"
+          exit
+        else [ "$environment" = "staging" ]
+          # specify which directory to copy from
+          echo -e ${white}"Enter the name of the s3 directory you want to copy files FROM and press [ENTER]: " ${cyan}
+          read custom
+          # copy files from the custom directory to the staging directory
+          aws s3 sync s3://docs.magedevteam.com/$custom/ $staging/ --exclude ".git/" --dryrun;
+          exit
+        fi
+        break;;
+        No)
+        echo -n -e ${white}"Enter the name of the s3 directory you want to deploy to production and press [ENTER]: " ${cyan}
+        read custom
+        # copy files from the custom directory to the production directory
+        aws s3 sync s3://docs.magedevteam.com/$custom/ $production/ --exclude ".git/" --dryrun;
+        echo -e ${green}"Deployment complete!"
+        exit 
+      esac
+    done
+  esac
+done
