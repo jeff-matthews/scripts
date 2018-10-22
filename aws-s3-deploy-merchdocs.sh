42927a041a3080f359f8d6950cf367b76ceec774
Jeff Matthews <matthews.jeffery@gmail.com>
2018-10-22 13:06:44 -0500

    Add option to list directories in staging

--- aws-s3-deploy-merchdocs.sh
+++ aws-s3-deploy-merchdocs.sh
@@ -51,7 +51,7 @@ select yn in "Yes" "No"; do
     Yes)
     if [ "$environment" = "personal" ]; then
       # deploy all local files from the current directory to your personal directory on the S3 staging bucket
-      echo -e "Enter the name of your personal s3 directory and press [ENTER]: "
+      echo -e "Enter the name of your personal s3 directory (no slashes) and press [ENTER]: "
       read -r name
       echo -e "Uploading the contents of the current local directory to your personal S3 staging environment..."
       # pass the s3 path ($name) to the aws s3 sync command
@@ -72,19 +72,41 @@ select yn in "Yes" "No"; do
       case $list in
         Yes)
         if [ "$environment" = "personal" ]; then
+          echo -e "Do you want to see the directory structure on staging?"
+            # This isn't very useful because there's no --max-depth option to limit the output of a recursive list. This is the best we can do right now.
+            select structure in "Yes" "No"; do
+              case $structure in
+                Yes)
+                  aws s3 ls s3://docs.magedevteam.com/;
+                break;;
+                No)
+                break;;
+              esac
+            done
           # specify which directory to copy FROM
-          echo -e "Enter the S3 SOURCE directory and press [ENTER]: "
+          echo -e "Enter the S3 SOURCE directory (no slashes) and press [ENTER]: "
           read -r custom
           # specify which directory to copy TO
-          echo -e "Enter the S3 DESTINATION directory and press [ENTER]: "
+          echo -e "Enter the S3 DESTINATION directory (no slashes) and press [ENTER]: "
           read -r name
           # copy files from the custom directory to the personal directory
           aws s3 sync "s3://docs.magedevteam.com/$custom/" "s3://docs.magedevteam.com/$name/" --dryrun;
           echo -e "Deployment complete!"
           exit
         else [ "$environment" = "staging" ]
+          echo -e "Do you want to see the directory structure on staging?"
+            # This isn't very useful because there's no --max-depth option to limit the output of a recursive list. This is the best we can do right now.
+            select structure in "Yes" "No"; do
+              case $structure in
+                Yes)
+                  aws s3 ls s3://docs.magedevteam.com/;
+                break;;
+                No)
+                break;;
+              esac
+            done
           # specify which directory to copy from
-          echo -e "Enter the S3 SOURCE directory and press [ENTER]: "
+          echo -e "Enter the S3 SOURCE directory (no slashes) and press [ENTER]: "
           read -r custom
           # copy files from the custom directory to the staging directory
           aws s3 sync "s3://docs.magedevteam.com/$custom/" "$staging/" --dryrun;
