a6836cdca5bba5b5928f09515d540cb01bd69694
Jeff Matthews <matthews.jeffery@gmail.com>
2022-03-15 15:59:13 -0500

    Added some prompts

--- directory-migration.sh
+++ directory-migration.sh
@@ -1,22 +1,29 @@
 #!/bin/sh
 
 # debugging
-set -x
+# set -x
 
 # set "fail on error" in bash
 set -e
 
-echo -e ${white}"Enter the full path to the working directory." ${cyan}
+red='\033[1;31m'
+green='\033[1;32m'
+yellow='\033[1;33m'
+purple='\033[1;34m'
+magenta='\033[1;35m'
+cyan='\033[1;36m'
+white='\033[1;37m'
+
+echo -e ${white}"Enter the full path to the working directory."
 read dir
- 
 cd $dir
+echo -e ${yellow}"The working directory is $dir."
 
-echo "Changed to $dir"
 
-for file in **/*.md *.md;
+# for file in **/*.md *.md;
 
-do 
-  dir=$(echo $file | cut -d. -f1);
-  mkdir -p $dir;
-  mv $file $dir/index.md;
-done
+# do 
+#   dir=$(echo $file | cut -d. -f1);
+#   mkdir -p $dir;
+#   mv $file $dir/index.md;
+# done
