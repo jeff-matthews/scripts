0c0c61148e275c94ccc7c603cc6cffe6dd6d3232
Jeff Matthews <matthews.jeffery@gmail.com>
2022-03-15 15:51:21 -0500

    Added initial working script

---
+++ directory-migration.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+
+echo “Enter dir”
+read dir
+ 
+cd $dir
+
+echo "Changed to $dir"
+ 
+# for file in *.md;
+
+# do
+#   mkdir -- "${file%.md}";
+#   mv -- "$file" "${file%.md}";
+# done
+
+for file in **/*.md *.md;
+
+do 
+  dir=$(echo $file | cut -d. -f1);
+  mkdir -p $dir;
+  mv $file $dir/index.md;
+done
