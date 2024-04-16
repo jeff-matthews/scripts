264c3cb4ef68a794abe14741406acadc2e8dd539
Jeff Matthews <jmatthew@adobe.com>
2024-04-16 17:09:07 -0500

    Added color and progress bar

--- doc-to-md.sh
+++ doc-to-md.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # Prompt the user for a file path
-echo "Enter the path to a .docx file:"
+echo -e "\033[0;33mEnter an absolute path to a .docx file:\033[0m"
 read filePath
 
 # Convert the .docx file to .md using pandoc
 mdPath="${filePath%.*}.md"
-pandoc -t markdown_strict --extract-media="./assets/" "$filePath" -o "$mdPath"
 
-echo "Converted .docx file to .md and saved it as $mdPath"
+# Get the size of the input file
+size=$(stat -f%z "$filePath")
+
+# Convert the file and display a progress bar
+pandoc -t markdown_strict --extract-media="./assets/" "$filePath" | pv -s "$size" > "$mdPath"
+
+echo -e "\033[0;32mConverted .docx file to .md and saved it as $mdPath\032[0m"
